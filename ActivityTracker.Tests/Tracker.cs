112e2977dff0b28d80ad27f010b914cc6debc88c
alnzwrt <semenov.9julia@gmail.com>
2025-12-07 12:42:38 +0200

    Red: Constructor test failing

---
+++ ActivityTracker.Tests/Tracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityTracker.Tests;
+
+public class Tracker
+{
+    public int DailyGoal { get; private set; }
+
+    public Tracker(int goal)
+    {
+        // Тут поки пусто
+    }
+}
---
+++ ActivityTracker.Tests/TrackerTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Xunit;
+namespace ActivityTracker.Tests;
+
+public class TrackerTests
+{
+    [Fact]
+    public void Constructor_ShouldSetDailyGoal()
+    {
+        int goal = 10000;
+
+        var tracker = new Tracker(goal);
+
+        Assert.Equal(goal, tracker.DailyGoal);
+    }
+}
